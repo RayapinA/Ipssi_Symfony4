12228bc60053907cd71919d3566f0e19bf5dcdb5
RayapinA <rayapin.antoine36@gmail.com>
2019-01-07 15:01:23 +0100

    Modification du template FirstArticle pour l'affichage

--- src/Entity/FirstArticle.php
+++ src/Entity/FirstArticle.php
@@ -16,9 +16,26 @@ class FirstArticle
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $name;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
     }
     
 }
