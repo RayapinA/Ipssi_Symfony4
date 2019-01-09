215c4b1c74c03659543f3e016b868f8f921f80dd
RayapinA <rayapin.antoine36@gmail.com>
2019-01-09 12:51:12 +0100

    Creation de l'enregistrement et la connexion d'un utilisateur

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,6 +6,7 @@ use App\Entity\FirstArticle;
 use App\Form\ArticleType;
 use App\Repository\FirstArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -46,36 +47,38 @@ class ArticleController extends AbstractController
     }   
     
      /**
-     * @Route("/article/{{name}}", name="article")
+     * @Route("/article/article",name="articleBy")
+     *     
+     * @ParamConverter("FirstArticle",options = {"mapping" = {"id" = "id"}})
      */
 
-    public function indexByName(Request $request, FirstArticleRepository $articleRepository)
+    public function indexByName(Request $request, FirstArticleRepository $articleRepository, FirstArticle $article)
     {
        $articles = $articleRepository->findAll();
 
-       $article = new FirstArticle();
-       $form = $this->createForm(ArticleType::class,$article);
-       $form->handleRequest($request);
+       //$article = new FirstArticle();
+    //    $form = $this->createForm(ArticleType::class,$article);
+    //    $form->handleRequest($request);
 
 
-       if($form->isSubmitted() &&  $form->isValid()){
-           $entityManager = $this->getDoctrine()->getManager();
-           $entityManager->persist($article);
-           $entityManager->flush();
+    //    if($form->isSubmitted() &&  $form->isValid()){
+    //        $entityManager = $this->getDoctrine()->getManager();
+    //        $entityManager->persist($article);
+    //        $entityManager->flush();
 
 
-           unset($entityManager);
-           unset($form);
-           $entityManager = $this->getDoctrine()->getManager();
-           $form = $this->createForm(ArticleType::class,$article);
+    //        unset($entityManager);
+    //        unset($form);
+    //        $entityManager = $this->getDoctrine()->getManager();
+    //        $form = $this->createForm(ArticleType::class,$article);
 
-           //$this->redirectToRoute('register success')
-       }
+    //        //$this->redirectToRoute('register success')
+    //    }
 
-       return $this->render('article/index.html.twig', [
+       return $this->render('article/oneArticle.html.twig', [
            'controller_name' => 'ArticleController',
-           'form' => $form->createView(),
            'articles' => $articles,
        ]);
+       
    }  
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,11 +6,12 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -48,9 +49,20 @@ class User
      */
     private $articles;
 
+    /**
+    *  @ORM\Column(type="simple_array")
+    */
+    private $roles;
+
+    /**
+    *  @ORM\Column()
+    */
+    private $password;
+
     public function __construct()
     {
         $this->articles = new ArrayCollection();
+        $this->roles = array("ROLE USER");
     }
 
     public function getId(): ?int
@@ -136,4 +148,38 @@ class User
 
         return $this;
     }
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+    public function setRoles($roles)
+    {
+        $this->roles = $roles;
+        return $this;
+    }
+    public function getSalt()
+    {
+        return null;
+    }
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    public function eraseCredentials():void
+    {
+        
+    }
+
+    
 }
