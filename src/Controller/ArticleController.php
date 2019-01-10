160135e8cfc098abfbb0de5b2feb6f1fdf9d7a44
RayapinA <rayapin.antoine36@gmail.com>
2019-01-10 15:26:55 +0100

    Mise en forme template OneArticle

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,12 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\FirstArticle;
-use App\Entity\User;
 use App\Form\ArticleType;
 use App\Repository\FirstArticleRepository;
-use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-// use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -17,7 +14,6 @@ class ArticleController extends AbstractController
     /**
      * @Route("/article", name="article")
      */
-
      public function index(Request $request, FirstArticleRepository $articleRepository)
      {
         $articles = $articleRepository->findAll();
@@ -51,33 +47,23 @@ class ArticleController extends AbstractController
      /**
      * @Route("/article/{id}",name="articleBy")
      */
-
-    public function indexByName(Request $request, FirstArticleRepository $articleRepository, FirstArticle $article, User $user)
+    public function indexByName(Request $request, FirstArticleRepository $articleRepository, FirstArticle $article)
     {
 
-    //    $form = $this->createForm(ArticleType::class,$article);
-    //    $form->handleRequest($request);
+       $form = $this->createForm(ArticleType::class,$article);
+       $form->handleRequest($request);
 
+       if($form->isSubmitted() &&  $form->isValid()){
+           $entityManager = $this->getDoctrine()->getManager();
+           $entityManager->persist($article);
+           $entityManager->flush();
+       }
 
-    //    if($form->isSubmitted() &&  $form->isValid()){
-    //        $entityManager = $this->getDoctrine()->getManager();
-    //        $entityManager->persist($article);
-    //        $entityManager->flush();
-
-
-    //        unset($entityManager);
-    //        unset($form);
-    //        $entityManager = $this->getDoctrine()->getManager();
-    //        $form = $this->createForm(ArticleType::class,$article);
-
-    //        //$this->redirectToRoute('register success')
-    //    }
- //dump($user);exit();
        return $this->render('article/oneArticle.html.twig', [
-           'controller_name' => 'ArticleController',
            'article' => $article,
-           'user' => $user
-       ]);
+           'form' => $form->createView(),
+       ]); 
+           
        
    }  
 }
